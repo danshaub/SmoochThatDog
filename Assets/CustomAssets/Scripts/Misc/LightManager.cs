e29c7135566bca224a750efdff7e0128a83601aa
Dan Haub <haub@chapman.edu>
2020-05-03 14:59:39 -0700

    Reverts to default renderer and adds light quality settings

---
+++ Assets/CustomAssets/Scripts/Misc/LightManager.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class LightManager : MonoBehaviour
+{
+    [System.Serializable]
+    public struct LightLevel
+    {
+        public GameObject[] lights;
+    }
+    public LightLevel[] lightLevels;
+    public int currentLightLevel;
+    public bool includeBaseLevelInDetail = false;
+    private bool previouslyIncluded = false;
+    private int previousLightLevel;
+    private void OnValidate()
+    {
+        currentLightLevel = Mathf.Clamp(currentLightLevel, 0, lightLevels.Length - 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Application.isPlaying)
+        {
+            if ((currentLightLevel != previousLightLevel) || (includeBaseLevelInDetail != previouslyIncluded))
+            {
+                SetLightLevel(currentLightLevel);
+            }
+
+            previouslyIncluded = includeBaseLevelInDetail;
+            previousLightLevel = currentLightLevel;
+        }
+    }
+
+    public void SetLightLevel(int newLightLevel)
+    {
+        currentLightLevel = Mathf.Clamp(newLightLevel, 0, lightLevels.Length - 1);
+
+        for (int i = 0; i < lightLevels.Length; i++)
+        {
+            //Set base light detail inactive if not included in higher detail levels
+            if (i == 0)
+            {
+                if (includeBaseLevelInDetail || currentLightLevel == 0)
+                {
+                    foreach (GameObject light in lightLevels[i].lights)
+                    {
+                        light.SetActive(true);
+                    }
+                }
+                else
+                {
+                    foreach (GameObject light in lightLevels[i].lights)
+                    {
+                        light.SetActive(false);
+                    }
+                }
+
+            }
+            //Set lights to active if they are within the current light level
+            else if (i <= currentLightLevel)
+            {
+                foreach (GameObject light in lightLevels[i].lights)
+                {
+                    light.SetActive(true);
+                }
+            }
+            //Otherwise turn them off
+            else
+            {
+                foreach (GameObject light in lightLevels[i].lights)
+                {
+                    light.SetActive(false);
+                }
+            }
+        }
+    }
+}
